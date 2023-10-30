d0c9d82351b0fa47060a9d230d3cdb8fecdcd5ba
Preethi Kurian <pkuttiani@gmail.com>
2023-10-30 18:54:00 +0530

    Updated disable click on btn before key press

--- game.js
+++ game.js
@@ -2,44 +2,48 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour;
 var gamePattern = [];
 var userClickedPattern = [];
-var started=false;
+var started = false;
 var level = 0;
 
-
+if (!started) {
+    document.getElementsByClassName("btn").disabled = true;
+}
 $(document).keypress(function (event) {
-    if(!started){
+    if (!started) {
         nextSequence();
         $("#level-info").hide();
-        started=true;
+        started = true;
+
     }
     // $("#level-title").text("Level "+ level);
-   
+
 });
 
 function nextSequence() {
+    
+
     userClickedPattern = [];
-   
     level++;
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
+    playSound(randomChosenColour);
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeTo(100, 0.3, function () { $(this).fadeTo(500, 1.0); });
-    playSound(randomChosenColour);
-
+    
 }
 
 
 $(".btn").click(function (e) {
-  
+if(started){
     var userChosenColor = e.target.id;
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
-    var index = userClickedPattern.length -1;
+    var index = userClickedPattern.length - 1;
 
     checkAnswer(index);
-
+}
 });
 
 function playSound(name) {
@@ -80,11 +84,13 @@ function checkAnswer(currentLevel) {
 
 
 }
-
-function startOver(){
-    started=false;
-    level=0;
-    gamePattern=[];
+if (!started) {
+    document.getElementsByClassName("btn").disabled = true;
+}
+function startOver() {
+    started = false;
+    level = 0;
+    gamePattern = [];
     $("#level-info").show();
 
-   }
+}
